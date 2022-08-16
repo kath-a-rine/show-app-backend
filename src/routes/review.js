142dbce173e2abdf9c6657fb85197b3bde5f433f
Elizabeth <elisha.elizabeth@gmail.com>
2022-08-16 11:32:19 -0700

    updates to routes

--- src/routes/review.js
+++ src/routes/review.js
@@ -1,5 +1,23 @@
 // CRUD routes for reviews
 
+'use strict';
+
+const express = require('express');
+const { reviewInterface } = require('../models');
+
+const router = express.Router();
+
 // POST review
+router.post('/review', async (req, res, next) => {
+  let review = req.body;
+
+  //query to the database ---****
+  let response = await reviewInterface.create(review);
+  res.status(200).send(response);
+});
 
 // GET all reviews
+router.get('/review', async (req, res, next) => {
+  let allReviews = await reviewInterface.readAll();
+  res.status(200).send(allReviews);
+});
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,23 @@
 // CRUD routes for users
 
+'use strict';
+
+const express = require('express');
+const { userInterface } = require('../models');
+
+const router = express.Router();
+
 // POST user
+router.post('/user', async (req, res, next) => {
+  let user = req.body;
+
+  let response = await userInterface.create(user);
+  res.status(200).send(response);
+});
 
 // GET one user
+router.get('/user/:id', async (req, res, next) => {
+  let { id } = req.params;
+  let oneUser = await animalsInterface.readOne(id);
+  res.status(200).send(oneUser);
+});
