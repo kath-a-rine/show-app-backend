069fbe91d3350ae5fab240903c1ee41d66a0245b
Elizabeth <elisha.elizabeth@gmail.com>
2022-08-16 11:03:23 -0700

    server up and running

---
+++ src/error-handlers/404.js
@@ -0,0 +1,11 @@
+function handle404(req, res, next) {
+
+  const errorObject = {
+    status: 404,
+    message: 'Page not found'
+  };
+
+  res.status(404).json(errorObject);
+}
+
+module.exports = handle404;
--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,7 @@
+'use strict';
 
-
+const { Sequelize } = require('sequelize');
+// require('dotenv').config();
 
 // database information
 
@@ -8,3 +10,22 @@
 
 
 // create our associations between tables - show id between Show and Review tables, user id between User and Review tables 
+
+// Connection to Database
+// const sequelize = new Sequelize(DATABASE_URL, {
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false,
+//     },
+//   },
+// });
+
+// Create tables
+
+// Create associations between tables
+
+
+// module.exports = {
+//   sequelize, 
+// };
