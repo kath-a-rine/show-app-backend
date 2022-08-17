df542039640688f752a3865db8714197e2125fad
Guy Farley <guy.e.farley@gmail.com>
2022-08-16 20:29:42 -0700

    Pulling data from MovieDB

--- src/routes/movieDb.js
+++ src/routes/movieDb.js
@@ -8,12 +8,14 @@ const axios = require('axios');
 require('dotenv').config();
 
 // showSchema: title, image, description, genre
-  
+
 const router = express.Router();
 
 //  get all movies
 router.get('/moviedb', async (req, res, next) => {
+  // let title = req.query.title;
   let title = req.query.title;
+  console.log(title);
 
   // let showObject = {
   //   title: name,
@@ -23,7 +25,7 @@ router.get('/moviedb', async (req, res, next) => {
   // }
 
   let allShows = await axios.get(`https://api.themoviedb.org/3/search/tv?api_key=${process.env.MOVIE_API_KEY}&language=en-US&include_adult=false&total_results=20&query=${title}`);
-  console.log('allShows', allShows);
+  console.log(`allShows matching ${title}: `, allShows.data.results);
   res.status(200).send(allShows.data.results);
 });
 
