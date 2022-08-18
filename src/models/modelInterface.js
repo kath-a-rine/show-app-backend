6aa86242c689b4f34c1d76214728987192321bc8
Katharine Swilley <kswilley@gmail.com>
2022-08-18 14:16:41 -0700

    updated interface

--- src/models/modelInterface.js
+++ src/models/modelInterface.js
@@ -71,18 +71,19 @@ class modelInterface {
       return err;
     }
   }
-}
 
-//   // delete
-//   async delete(id) {
-//     try{
-//       let deletedInstance = await this.model.findOne({where: { id }});
-//       await this.model.destroy({where: { id }});
-//       return deletedInstance;
-//     } catch (err) {
-//       console.error(err);
-//       return err;
-//     }
-//   }
+
+// delete
+  async delete(id){
+    try{
+      let deletedInstance = await this.model.findOne({where: { id }});
+      await this.model.destroy({where: { id }});
+      return deletedInstance;
+    } catch (err) {
+      console.error(err);
+      return err;
+    }
+  }
+}
 
 module.exports = modelInterface;
